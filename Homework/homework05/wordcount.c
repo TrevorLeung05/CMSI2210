ecf1fb4f230f8bb153b2df9f4060ab0ff0a320f0
Daniel Lee <dlee109@lion.lmu.edu>
2024-10-30 02:56:24 -0700

    wordcount.c uploaded

---
+++ Homework/homework05/wordcount.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    //Open the file
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    int word_count = 0;
+    int in_word = 0;
+    int ch;
+
+    //Read through the file, checking character by character
+    while ((ch = fgetc(file)) != EOF) {
+        //Check to see if the current character is a whitespace character
+        if (isspace(ch)) {
+            if (in_word) {
+                //End of a word
+                in_word = 0;
+            }
+        } else {
+            if (!in_word) {
+                //Start of a new word
+                word_count++;
+                in_word = 1;
+            }
+        }
+    }
+
+    //Close the file
+    fclose(file);
+
+    //Outputs the word count
+    printf("Word Count: %d\n", word_count);
+
+    return 0;
+}
