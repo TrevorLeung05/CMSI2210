3518e42418a7b782daf809322ac8cedc3219908f
TrevorLeung05 <143565808+TrevorLeung05@users.noreply.github.com>
2024-12-04 01:41:29 -0800

    Update whichEndIsUp.c

--- Homework/homework06/whichEndIsUp.c
+++ Homework/homework06/whichEndIsUp.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+void checkEndianess() {
+    unsigned int x = 1;
+    char *c = (char*)&x;
+
+    if (*c) {
+        printf("This system is Little-Endian.\n");
+    } else {
+        printf("This system is Big-Endian.\n");
+    }
+}
+
+int main() {
+    printf("Checking the Endianness of your system...\n");
+    checkEndianess();
+    return 0;
+}
 
